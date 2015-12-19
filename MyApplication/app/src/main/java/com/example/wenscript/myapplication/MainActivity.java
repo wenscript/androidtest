554c100a6d8685b2f3eaf04fa65d8517d4aba0f6
wenscript <534727769@qq.com>
2015-12-19 20:09:59 +0800

    complete

--- MyApplication/app/src/main/java/com/example/wenscript/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/wenscript/myapplication/MainActivity.java
@@ -1,52 +1,46 @@
 package com.example.wenscript.myapplication;
 
+import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
+    本例讲的是ClipDrawable的使用
+     */
+    private ImageView imageView;
+    private int level=0;
+    private ClipDrawable cd;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what==0x1234){
+                cd.setLevel(level);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        imageView= (ImageView) findViewById(R.id.beauty);
+        cd= (ClipDrawable) imageView.getDrawable();
+        final Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                level=level+500;
+                handler.sendEmptyMessage(0x1234);
+                if (level>10000){
+                    timer.cancel();
+                }
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
+        },0,300);
+    }}
